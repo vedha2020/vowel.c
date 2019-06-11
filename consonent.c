13bf00a4e034741d398a6636e818b0fb8697b4ec
vedha2020 <51699639+vedha2020@users.noreply.github.com>
2019-06-11 19:09:10 +0530

    Create consonent.c

---
+++ consonent.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<stdlib.h>
+int main()
+{
+	char c;
+	int vowel1,vowel2;
+	scanf("%c",&c);
+	vowel1=(c=='a'||c=='e'||c=='i'||c=='o'||c=='u');
+	vowel2=(c=='A'||c=='E'||c=='I'||c=='O'||c=='U');
+	if(vowel1||vowel2)
+	printf("%c Vowel",c);
+	else
+	printf("%c Consonant",c);
+	return 0;
+}
