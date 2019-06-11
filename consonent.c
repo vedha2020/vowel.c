99ae7696b9bd5e71bb1682108da5be75a672e234
vedha2020 <51699639+vedha2020@users.noreply.github.com>
2019-06-11 19:20:13 +0530

    Update consonent.c

--- consonent.c
+++ consonent.c
@@ -3,12 +3,15 @@
 int main()
 {
 	char c;
-	int vowel1,vowel2;
+	int vowel1,vowel2,vowel3;
 	scanf("%c",&c);
 	vowel1=(c=='a'||c=='e'||c=='i'||c=='o'||c=='u');
 	vowel2=(c=='A'||c=='E'||c=='I'||c=='O'||c=='U');
+	vowel3=(c=='!'||c=='@'||c=='#'||c=='$'||c=='%');
 	if(vowel1||vowel2)
 	printf("Vowel");
+	else if(vowel3=vowel3)
+	printf("invalid");
 	else
 	printf("Consonant");
 	return 0;
